f35af346756134f2a2824e2d817a59f938f89ff0
Deo <diothewind@gmail.com>
2024-12-24 16:51:58 +0800

    Add functionality to retrieve definitions from an API

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
+});
+
+app.post("/definitions", async (req, res) => {
+    try {
+        const word = req.body.word;
+        const definitions = await axios.get(API_URL + word);
+
+        res.render("definition.ejs", {
+            definitions: JSON.stringify(definitions.data[0])
+        });
+    } catch (error) {
+        res.status(500).send("Error retrieving definitions");
+    }
+
 });
 
 app.listen(port, () => {
